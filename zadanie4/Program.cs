47d9a9b2af77c9f73066a1591eecdbc3bd3b5de3
leenarie <KATIDARK@MAIL.RU>
2020-05-18 21:12:31 +0500

    Добавьте файлы проекта.

---
+++ zadanie4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList big = new ArrayList();
+            ArrayList newc = new ArrayList();
+            ArrayList collection = new ArrayList() { 3, 'a', 8, 'B', 4.5, 'i', 55, -4, 'H', 'c', -9.01, 77, 'J', 14, 0.2};
+            foreach (object o in collection)
+            {
+
+                if (o is char)
+                {
+                    if (char.IsUpper(Convert.ToChar(o)))
+                    {
+                        big.Add(o);
+                    }
+                }
+
+                if (((o is int) || (o is double)))
+                {
+                    newc.Add(o);
+                }
+            }
+            foreach (object o in collection)
+            {
+                Console.Write(o + " ");
+            }
+            Console.WriteLine();
+            foreach (object o in newc)
+            {
+                Console.Write(o + " ");
+            }
+            Console.WriteLine();
+            foreach (object o in big)
+            {
+                Console.Write(o + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
